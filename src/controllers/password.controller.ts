5407b2f31f5e8b481adef4c818497a07788650c3
quy.hoang <quy.hoang@kiaisoft.com.vn>
2024-07-09 09:03:58 +0700

    feat: forgot password

---
+++ src/controllers/password.controller.ts
@@ -0,0 +1,46 @@
+import crypto from 'crypto';
+
+import { emailSchema } from '@auth/schemes/password';
+import { getAuthUserByUsernameOrEmail, updatePasswordToken } from '@auth/services/auth.service';
+import { BadRequestError, IAuthDocument, IEmailMessageDetails } from '@quysterben/jobber-shared';
+import { NextFunction, Request, Response } from 'express';
+import { config } from '@auth/config';
+import { publishDirectMessage } from '@auth/queues/auth.producer';
+import { authChannel } from '@auth/server';
+import { StatusCodes } from 'http-status-codes';
+
+export async function createForgotPassword(req: Request, res: Response, next: NextFunction): Promise<void> {
+  try {
+    const { error } = await Promise.resolve(emailSchema.validate(req.body));
+    if (error?.details) {
+      throw new BadRequestError(error.details[0].message, 'Password create() method error');
+    }
+    const { email } = req.body;
+    const existingUser: IAuthDocument | undefined = await getAuthUserByUsernameOrEmail(email);
+    if (!existingUser) {
+      throw new BadRequestError('User not found.', 'Password create() method error');
+    }
+    const randomBytes: Buffer = await Promise.resolve(crypto.randomBytes(20));
+    const randomCharacters: string = randomBytes.toString('hex');
+    const date: Date = new Date();
+    date.setHours(date.getHours() + 1);
+    await updatePasswordToken(existingUser.id!, randomCharacters, date);
+    const resetLink = `${config.CLIENT_URL}/reset-password/${randomCharacters}`;
+    const messageDetails: IEmailMessageDetails = {
+      receiverEmail: email,
+      resetLink,
+      username: existingUser.username!,
+      template: 'forgetPassword'
+    };
+    await publishDirectMessage(
+      authChannel,
+      'email-notification',
+      'auth-email',
+      JSON.stringify(messageDetails),
+      'Forget password message sent to Notification Service'
+    );
+    res.status(StatusCodes.OK).json({ message: 'Reset password link sent to your email.' });
+  } catch (error) {
+    next(error);
+  }
+}
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,3 +1,4 @@
+import { createForgotPassword } from '@auth/controllers/password.controller';
 import { read } from '@auth/controllers/signin.controller';
 import { create } from '@auth/controllers/signup.controller';
 import { update } from '@auth/controllers/verify-email.controller';
@@ -9,6 +10,7 @@ export function authRoutes(): Router {
   router.post('/signup', create);
   router.post('/signin', read);
   router.put('/verify-email', update);
+  router.put('/forgot-password', createForgotPassword);
 
   return router;
 }
